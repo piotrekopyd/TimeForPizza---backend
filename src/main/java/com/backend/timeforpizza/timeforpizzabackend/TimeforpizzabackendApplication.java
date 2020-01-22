3a92e3f167bf0a1cbea9d9c72ae9859b572a3f2a
Piotr Opyd <opydpiotrek@gmail.com>
2020-01-22 17:41:41 +0100

    Init commit

---
+++ src/main/java/com/backend/timeforpizza/timeforpizzabackend/TimeforpizzabackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.timeforpizza.timeforpizzabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeforpizzabackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeforpizzabackendApplication.class, args);
+    }
+
+}
